f90ed3f6560b055548cae9cf32e1c95a2c78a6bb
josediazangarita <josediazangarita@gmail.com>
2023-11-16 03:04:22 -0300

    Creación del Productos.map con los contenedores padre e hijos que incluyen nombre del producto, imagen, precio, descripción y botón de enlace al detalle

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,14 +1,30 @@
+import { useEffect, useState } from "react"
 import { mFetch } from "../../Helpers/mFetch"
 
 export const ItemListContainer = ({ greeting }) => {
-  // Llamada a mi promesa
-  mFetch()
-    .then(result => console.log(result)) 
+  const [ productos, setProductos ] = useState([])
+  // Llamada a mi promesa mock de una API
+  useEffect(()=>{
+    mFetch()
+    .then(result => setProductos (result)) 
     .catch(err => console.log(err))
-
+  }, [])
+  console.log(productos)
   return (
     <div>
       <h2 className="text-center"> { greeting } </h2>
+      {productos.map (product => <div key={product.id} className="card w-25">
+                                    <div className="card-body">
+                                      <img src={product.imgUrl} className="w-100" alt="Imagen de Tarjeta de Suscripción" />
+                                      <h3>{product.name}</h3>
+                                      <p>Precio:{product.precio}</p>
+                                      <p>Descripción:{product.description}</p>
+                                    </div>
+                                    <div className="card-footer">
+                                      <button className="btn btn-outline-dark w-100">Detalle</button>
+                                    </div>
+                                  </div>
+      )}
     </div>
   )
 }
