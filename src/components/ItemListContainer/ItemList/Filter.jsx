bdbca6c4f7055ee6503ea3e588cd2f9e010f61c6
josediazangarita <josediazangarita@gmail.com>
2023-11-17 16:41:58 -0300

    Creación del fitro de productos en tiempo real

---
+++ src/components/ItemListContainer/ItemList/Filter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+
+export const Filter = ({children, productos}) => {
+    const [filterState, setFilterState] = useState ("")
+    
+    const handleFilterChange = (e) => {
+      //<input type="tipo" name="apellido"
+      setFilterState(e.target.value)
+    }
+    
+    return (
+      // Children es productFiltered
+      children({productos, filterState, handleFilterChange})
+    )
+  }
+
+
--- src/components/ItemListContainer/ItemList/ItemList.jsx
+++ src/components/ItemListContainer/ItemList/ItemList.jsx
@@ -1,14 +1,27 @@
+import { Filter } from "./Filter"
 
-export const ItemList = ({productos}) => {
-  return (
-    <div style={{
+const productFiltered = ({productos, filterState, handleFilterChange}) => (
+<>
+  <div>
+    <label>Buscar</label>
+    <input 
+      className="form-control" 
+      type="text" 
+      value={filterState}
+      onChange={handleFilterChange}
+    />
+  </div>
+  <br />
+  <div style={{
         display: "flex",
         flexDirection: "row",
         flexWrap: "wrap",
         gap: "25px",
         justifyContent: "center"
       }}>
-        {productos.map (product => <div key={product.id} className="card w-25">
+        {filterState === "" 
+          ?
+          productos.map (product => <div key={product.id} className="card w-25">
                                       <div className="card-body p-0 text-center">
                                         <img src={product.imgUrl} className="w-100" alt="Imagen de Tarjeta de Suscripción" />
                                         <h5>{product.name}</h5>
@@ -20,7 +33,36 @@ export const ItemList = ({productos}) => {
                                         <button className="btn btn-outline-dark w-100">Detalle</button>
                                       </div>
                                     </div>
-        )}
-      </div>
+            )
+          :
+
+          productos
+          .filter(prod => prod.name.toLowerCase().includes(filterState))
+          .map (product => <div key={product.id} className="card w-25">
+                                          <div className="card-body p-0 text-center">
+                                            <img src={product.imgUrl} className="w-100" alt="Imagen de Tarjeta de Suscripción" />
+                                            <h5>{product.name}</h5>
+                                            <p>Precio:{product.precio}</p>
+                                            <p>Stock:{product.stock}</p>
+                                            <p>Descripción:{product.description}</p>
+                                          </div>
+                                          <div className="card-footer">
+                                            <button className="btn btn-outline-dark w-100">Detalle</button>
+                                          </div>
+                                        </div>
+                )
+        }
+  </div>
+</>
+)
+
+
+export const ItemList = ({productos}) => {
+  return (
+    <Filter productos={productos}>
+        {productFiltered}
+    </Filter>
   )
 }
+
+
